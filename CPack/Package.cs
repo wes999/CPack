435ee04ef873bf8f274b313fefd30950a22a9ddc
wesmac <wesmac8989@gmail.com>
2022-02-25 09:56:44 -0400

    initial commit

---
+++ CPack/Package.cs
@@ -0,0 +1,12 @@
+﻿namespace CPack
+{
+    public class Package
+    {
+        public string Name { get; set; }
+        public string Version { get; set; }
+        public string IncludePath { get; set; }
+        public string Description { get; set; }
+
+        public List<string> LibraryFiles { get; set; }
+    }
+}
---
+++ CPack/Program.cs
@@ -0,0 +1,46 @@
+﻿using PowerArgs;
+
+namespace CPack
+{
+    [ArgExceptionBehavior(ArgExceptionPolicy.StandardExceptionHandling)]
+    public class CPack
+    {
+        public Package Package { get; set; }
+
+        [ArgActionMethod, ArgDescription("Intializes Package")]
+        public void Init()
+        {
+            Package = new Package();
+            Console.WriteLine("Package Name:");
+            Package.Name = Console.ReadLine(); 
+
+            Console.WriteLine("Package Description:");
+            Package.Description = Console.ReadLine();
+
+            Console.WriteLine("Package Include Directory:");
+            Package.IncludePath = Console.ReadLine();
+
+            Console.WriteLine("Include Directory");
+
+            string[] files = Directory.GetFiles(Console.ReadLine());
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                if (files[i].EndsWith(".lib"))
+                {
+                    Package.LibraryFiles.Add(files[1]);
+                }
+            }
+
+
+        }
+    }
+
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
