b492f77c26d493e07b8651db2506713d87e6681b
wesmac <wesmac8989@gmail.com>
2022-03-24 19:17:17 -0300

    started adding networking capabilities

--- CPack/Networking.cs
+++ CPack/Networking.cs
@@ -1,15 +1,31 @@
 ﻿using System.Net;
+using Newtonsoft.Json;
 
 namespace CPack
 {
     public static class Networking
     {
-        public static async Task Upload(string url)
+        public static async Task GetPackages()
         {
             HttpClient client = new HttpClient();
 
-            byte[] bytes = await client.GetByteArrayAsync(url);
-            await File.WriteAllBytesAsync("", bytes);
+            string data = await client.GetStringAsync("localhost:7100/packages");
+
+            List<PackageForm> packages = JsonConvert.DeserializeObject<List<PackageForm>>(data);
         }
+    }
+
+    class PackageForm
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Version { get; set; }
+        public string IncludePath { get; set; }
+        public string Description { get; set; }
+        public string LibPath { get; set; }
+        public string BinPath { get; set; }
+
+        public DateTime LastUpDateTime { get; set; }
+        public DateTime CreationTime { get; set; }
     }
 }
